80d45e8dea6fc6cf9c751b470256a1eef64ea82c
damien <dfe@connecthings.com>
2020-01-22 17:59:33 +0100

    fix

--- testNotification/Notification/AppDelegate.swift
+++ testNotification/Notification/AppDelegate.swift
@@ -1,36 +1,25 @@
 //
 //  AppDelegate.swift
-//  Complete
+//  testNotifications
 //
-//  Created by Connecthings on 26/01/2017.
-//  Copyright © 2017 R&D connecthings. All rights reserved.
+//  Created by Damien on 17/01/2020.
+//  Copyright © 2020 Connecthings. All rights reserved.
 //
 
 import UIKit
-import ConnectPlaceActions
-import HerowConnection
-import HerowLocationDetection
-import UserNotifications
-import ConnectPlaceCommon
+import HerowConnection;
+import ConnectPlaceCommon;
 @UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
-class AppDelegate: NSObject, UIApplicationDelegate, HerowReceiveNotificationContentDelegate {
-    private let notificationIdentifier: String = "INVITE_CATEGORY"
-    var window: UIWindow?
-    var herowInitializer: HerowInitializer?
-    var herowdetectionManager: HerowDetectionManager?
+
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        herowInitializer = HerowInitializer.shared
-        herowInitializer?.configPlatform(Platform.preProd)
-                         .configApp(identifier: "your id", sdkKey: "your sdk key")
-                         .synchronize()
 
-
+        let herowInitializer =  HerowInitializer.shared
+        herowInitializer.configPlatform(Platform.prod).configApp(identifier: "mobilise", sdkKey: "9GZL4luvP4klY4uFB103").synchronize()
         if #available(iOS 10.0, *) {
             let center = UNUserNotificationCenter.current()
-            //The request can be done as well in a viewController which allows to display a message if the user refuse to receive notifications
-            
             center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
                 if (error == nil) {
                     NSLog("request authorization succeeded!");
@@ -39,18 +28,24 @@ class AppDelegate: NSObject, UIApplicationDelegate, HerowReceiveNotificationCont
         } else if(UIApplication.instancesRespond(to: #selector(UIApplication.registerUserNotificationSettings(_:)))){
             UIApplication.shared.registerUserNotificationSettings(UIUserNotificationSettings (types: [.alert, .sound], categories: nil))
         }
-
+        GlobalLogger.shared.startDebug()
         return true
     }
 
-    func application(_ application: UIApplication, didReceive notification: UILocalNotification) {
-        herowdetectionManager?.didReceivePlaceNotification(notification.userInfo)
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func didReceivePlaceNotification(_ placeNotification: HerowPlaceNotification) {
-        NSLog("open a controller with a place notification")
-        //Quick way to notify controller
-        let placeContentUserInfo:[String: PlaceNotification] = ["placeNotification": placeNotification]
-        NotificationCenter.default.post(name: NSNotification.Name("placeNotification"), object:nil, userInfo: placeContentUserInfo)
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+
+
 }
+
